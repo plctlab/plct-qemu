274157d5ab42facd363c3be7013165ccc7ff68e5
Weiwei Li <liweiwei@iscas.ac.cn>
2023-05-29 18:18:19 +0800

    target/riscv: Remove check on mode for MPRV
    Normally, MPRV can be set to 1 only in M mode (It will be cleared
    when returning to lower-privilege mode by MRET/SRET).
    
    Signed-off-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Signed-off-by: Junqiang Wang <wangjunqiang@iscas.ac.cn>

--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -44,7 +44,7 @@ int riscv_cpu_mmu_index(CPURISCVState *env, bool ifetch)
     if (!ifetch) {
         uint64_t status = env->mstatus;
 
-        if (mode == PRV_M && get_field(status, MSTATUS_MPRV)) {
+        if (get_field(status, MSTATUS_MPRV)) {
             mode = get_field(env->mstatus, MSTATUS_MPP);
             virt = get_field(env->mstatus, MSTATUS_MPV) &&
                    (mode != PRV_M);
